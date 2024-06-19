95c7a0c922755be434bd6f90c85736645736588d
Maximus7474 <the.maximus.7474@gmail.com>
2024-06-19 13:06:15 +0200

    rf: logger to handle more arguments

--- Bot Inscription (JS)/src/utils/logger.js
+++ Bot Inscription (JS)/src/utils/logger.js
@@ -4,20 +4,20 @@ class Logger {
     constructor(origin) {
         this.origin = origin
     }
-    info(message) {
-        console.log(`${colors.gray((new Date()).toLocaleString())} ${colors.cyan(`[${this.origin}] [INFO]`)} ${message}`);
+    info(message, ...args) {
+        console.log(`${colors.gray((new Date()).toLocaleString())} ${colors.cyan(`[${this.origin}] [INFO]`)} ${message}`, ...args);
     }
 
-    success(message) {
-        console.log(`${colors.gray((new Date()).toLocaleString())} ${colors.green(`[${this.origin}] [SUCCESS]`)} ${message}`);
+    success(message, ...args) {
+        console.log(`${colors.gray((new Date()).toLocaleString())} ${colors.green(`[${this.origin}] [SUCCESS]`)} ${message}`, ...args);
     }
 
-    warn(message) {
-        console.warn(`${colors.gray((new Date()).toLocaleString())} ${colors.yellow(`[${this.origin}] [WARN]`)} ${message}`);
+    warn(message, ...args) {
+        console.warn(`${colors.gray((new Date()).toLocaleString())} ${colors.yellow(`[${this.origin}] [WARN]`)} ${message}`, ...args);
     }
 
-    error(message) {
-        console.error(`${colors.gray((new Date()).toLocaleString())} ${colors.red(`[${this.origin}] [ERROR]`)} ${message}`);
+    error(message, ...args) {
+        console.error(`${colors.gray((new Date()).toLocaleString())} ${colors.red(`[${this.origin}] [ERROR]`)} ${message}`, ...args);
     }
 }
 
