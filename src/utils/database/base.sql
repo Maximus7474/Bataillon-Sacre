06c972cbf740ddfdf78911e04ab900fe58ee92ef
Maximus7474 <the.maximus.7474@gmail.com>
2025-02-09 15:57:13 +0100

    refactor(base.sql): removed users table

--- src/utils/database/base.sql
+++ src/utils/database/base.sql
@@ -1,14 +1,3 @@
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT,
-    discord_id TEXT UNIQUE,
-    signup_date INTEGER DEFAULT (strftime('%s', 'now')),
-    joined_date INTEGER,
-    email TEXT,
-    game_identifiers TEXT,
-    reglement TEXT
-);
-
 CREATE TABLE IF NOT EXISTS `forum-roles` (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     post_id TEXT UNIQUE NOT NULL,
