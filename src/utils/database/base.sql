e29c2bafe06cf6c352aeaf27c9ffbce7d06bf651
Maximus7474 <the.maximus.7474@gmail.com>
2025-05-29 17:40:36 +0200

    feat(base.sql): table definitions for events

--- src/utils/database/base.sql
+++ src/utils/database/base.sql
@@ -5,3 +5,27 @@ CREATE TABLE IF NOT EXISTS `forum-roles` (
     added INTEGER DEFAULT (strftime('%s', 'now')),
     added_by TEXT DEFAULT '{ "username": "SYSTEM", "id": "SYSTEM" }'
 );
+
+CREATE TABLE IF NOT EXISTS upcoming_events (
+    `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `thread_id` TEXT NUT NULL,
+    `message_id` TEXT NUT NULL,
+
+    `added` INTEGER DEFAULT (strftime('%s', 'now')),
+    `added_by` TEXT NOT NULL,
+    
+    `title` TEXT NOT NULL,
+    `image` TEXT DEFAULT NULL,
+    `date` INTEGER NOT NULL,
+    `duration` INTEGER DEFAULT 1,
+    `description` TEXT DEFAULT "",
+    `location` TEXT DEFAULT "N/A"
+);
+
+CREATE TABLE IF NOT EXISTS event_participants (
+    `event_id` INTEGER NOT NULL,
+    `updated` INTEGER DEFAULT (strftime('%s', 'now')),
+    `user_id` TEXT NOT NULL,
+    `participating` INTEGER DEFAULT 0,
+    PRIMARY KEY (event_id, user_id)
+);
