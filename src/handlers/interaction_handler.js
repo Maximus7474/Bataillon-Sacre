d06ef8165745d73e0b2c92999ebe4d9787a0f81b
Maximus7474 <the.maximus.7474@gmail.com>
2025-05-29 17:40:18 +0200

    chore(interaction_handler): added redirection to game event handler if customId is appropriate

--- src/handlers/interaction_handler.js
+++ src/handlers/interaction_handler.js
@@ -1,4 +1,5 @@
 const { GetInteractionHandlersForStaticMessages } = require('../utils/initialisation/setup_staticMessages');
+const { handleEventParticipation } = require('./custom/game_event_handler');
 const { checkSignupInteractionID, handleSignupResponse } = require('./custom/signup_handler');
 
 const log = new require('../utils/logger.js');
@@ -10,6 +11,8 @@ module.exports = async (client,interaction) => {
     const { customId } = interaction;
 
     if (customId.startsWith('collector')) return;
+
+    if (customId.startsWith('event_participation')) return handleEventParticipation(client, interaction);
 
     if (typeof callbacks[customId] === 'function') {
         logger.info('Running callbacks', customId);
