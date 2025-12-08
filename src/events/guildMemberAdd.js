8f901f29630cfc0a7899a748d697931c21ab66b1
Maximus7474 <94017712+Maximus7474@users.noreply.github.com>
2025-12-08 21:58:58 +0100

    fix(events/guildMemberAdd): fetch role asynchronously

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -14,7 +14,7 @@ module.exports = {
         if (guild.id !== guild_id) return;
 
         for (const roleId of roles.visiteur) {
-            const role = guild.roles.fetch(roleId);
+            const role = await guild.roles.fetch(roleId);
             if (role) {
                 member.roles.remove(role)
                 .catch(err => logger.error(`Unable to add role ${role.name} (${roleId}) to @${user.username} (${user.id}):\n`, err));
